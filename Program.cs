02c17f7dba32a2e04be756d5e0f9ee0d1053579f
SachekDenis <sachek.denis@gmail.com>
2020-04-08 15:11:24 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RulesOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rules = new Dictionary<string, string>()
+            {
+                {"D","&"},
+                {"Z","+"},
+                {"I","ID|D"},
+                {"R","I"},
+                {"V","VD"},
+                {"H","(A)|R|U"},
+                {"U","U^H|H"},
+                {"T","U"},
+                {"A","AZT|ZT|T"},
+                {"B","A"},
+                {"A","S"}
+            };
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
